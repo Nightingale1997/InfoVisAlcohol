f8e7cacf12e9fd9255acd4c900bc95dcac39e443
lucarl <carl.e.w.lundborg@gmail.com>
2020-02-28 10:50:10 +0100

    added alcohol charts

--- js/graphs.js
+++ js/graphs.js
@@ -10,3 +10,45 @@ var chart = bb.generate({
     // Hide all legend.
 });
 chart.legend.hide();
+
+var chart2 = bb.generate({
+  data: {
+    columns: [
+	["Male", 17.4, 13.2, 17.6, 20.6, 18.3],
+	["Female", 6.1, 4.6, 6.2, 7.2, 4.7],
+  ["Both sexes", 12.5, 9.4, 12.8, 14.8, 11.3]
+    ],
+    type: "bar"
+  },
+  bar: {
+    width: {
+      ratio: 0.5
+    }
+  },
+  axis: {
+    labels: true,
+    x: {
+      type: 'category',
+      tick: {
+        format: function(index) {
+          if(index === 0) {
+            return "Sweden";
+          } 
+          if(index === 1) {
+            return "Norway";
+          } 
+          if(index === 2) {
+            return "Iceland";
+          } 
+          if(index === 3) {
+            return "Finland";
+          } 
+          if(index === 4) {
+            return "Europe";
+          } 
+        }
+      }
+    }
+  },
+  bindto: "#barChart"
+}); 
