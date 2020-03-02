cab2843289fecf53a7425265d3000887184d6946
Kevin Solovjov <kevin.solovjov@gmail.com>
2020-03-02 20:34:30 +0100

    Europe fixed

--- js/main.js
+++ js/main.js
@@ -17,24 +17,60 @@ $.fn.isOnScreen = function () {
 
 };
 
+var animationtrigger = true;
+
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($('#consumption').isOnScreen()) {
-            // The element is visible, do something
-            $(".wrapper, .beercontainer").css("animation", "tilt 10s forwards");
-            $(".wrapper .beercontainer .glass .drink1").css("animation", "pour 10s forwards");
-            $(".wrapper .beercontainer .glass .drink2").css("animation", "pour2 10s forwards");
-            $(".wrapper .beercontainer .glass .drink3").css("animation", "pour3 10s forwards");
-            $(".wrapper .beercontainer .glass .drink4").css("animation", "pour4 10s forwards");
-            $(".wrapper .beercontainer .glass .drink5").css("animation", "pour5 10s forwards");
-        } else {
-            // The element is NOT visible, do something else
+        if (animationtrigger) {
+            if ($('#consumption').isOnScreen()) {
+                animationtrigger = false;
+                // The element is visible, do something
+                $(".wrapper, .beercontainer").css("animation", "tilt 10s forwards");
+                $(".wrapper .beercontainer .glass .drink1").css("animation", "pour 10s forwards");
+                $(".wrapper .beercontainer .glass .drink2").css("animation", "pour2 10s forwards");
+                $(".wrapper .beercontainer .glass .drink3").css("animation", "pour3 10s forwards");
+                $(".wrapper .beercontainer .glass .drink4").css("animation", "pour4 10s forwards");
+                $(".wrapper .beercontainer .glass .drink5").css("animation", "pour5 10s forwards");
+                $("#worldnumber").animate({
+                    opacity: 1
+                }, 4650);
+                setTimeout(
+                    function () {
+                        $(".drink1").css("animation", "none");
+
+                        $(".drink1").css("bottom", "-78.5755814px");
+                    }, 4650);
+            }
         }
+
     });
 
+    $("#worldboth").click(function () {
+        $(".drink1").css("bottom", "-78.5755814px");
+        $("#worldboth").addClass("active");
+        $("#worldmen").removeClass("active");
+        $("#worldwomen").removeClass("active");
+        $("#worldnumbertext").text("9.45");
+    });
     $("#worldmen").click(function () {
-        $(".drink1").css("transition", "2s ease");
-        $(".drink1").css("height", "179.6511628px");
+        $(".drink1").css("bottom", "-33.22674419px");
+
+        $("#worldboth").removeClass("active");
+        $("#worldmen").addClass("active");
+        $("#worldwomen").removeClass("active");
+        $("#worldnumbertext").text("15.45");
+    });
+    $("#worldwomen").click(function () {
+        $(".drink1").css("bottom", "-122.0348837px");
+
+        $("#worldboth").removeClass("active");
+        $("#worldmen").removeClass("active");
+        $("#worldwomen").addClass("active");
+        $("#worldnumbertext").text("3.7");
+    });
+
+    $("#finlandboth").click(function () {
+        $(".drink1").css("bottom", "-69.12790698px");
     });
 
 });
--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
 google.charts.load('current', {
-        'packages':['geochart'],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-        'mapsApiKey': 'AIzaSyDYiFq7i8Oo4f93jN7jQCfxXGFg_aDro-w'
-      });
+    'packages': ['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyDYiFq7i8Oo4f93jN7jQCfxXGFg_aDro-w'
+});
 
-      google.charts.setOnLoadCallback(drawRegionsMap);
+google.charts.setOnLoadCallback(drawRegionsMap);
 
-      function drawRegionsMap() {
-        var data = google.visualization.arrayToDataTable([
+function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
           ['Country', 'Monopolies since'],
           ['Sweden', 1905],
           ['Norway', 1919],
@@ -16,14 +16,18 @@ google.charts.load('current', {
           ['Finland', 1933]
         ]);
 
-        var options = {
-         region: '154',
-         colorAxis: {colors: ['#42b3f5', '#217aad', '#0f4b6e']},
-         datalessRegionColor: 'white',
-         legend: {numberFormat:'#'}
-        };
+    var options = {
+        region: '154',
+        colorAxis: {
+            colors: ['#42b3f5', '#217aad', '#0f4b6e']
+        },
+        datalessRegionColor: 'white',
+        legend: {
+            numberFormat: '#'
+        }
+    };
 
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-        chart.draw(data, options);
-      }
+    chart.draw(data, options);
+}
